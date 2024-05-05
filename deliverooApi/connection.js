aa2a1d2709b9180814f8fbfac423707d0914d10a
StefanoSacchet <104228685+StefanoSacchet@users.noreply.github.com>
2024-05-05 11:35:57 +0200

    Delete deliverooApi directory

--- deliverooApi/connection.js
+++
@@ -1,6 +0,0 @@
-import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
-
-export const client = new DeliverooApi(
-    "http://localhost:8080",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImU0MjBhZjRmODAyIiwibmFtZSI6InN0ZSIsImlhdCI6MTcxNDU3ODQ1OX0.j6hv5sWvFWNduAsDLNX9IOo7Wwh-72TBljYmu-IGupQ"
-);
