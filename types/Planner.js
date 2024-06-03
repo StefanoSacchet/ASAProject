74d5aa0a01101cc089846d1ed4653010226b8884
StefanoSacchet <sacchet13@gmail.com>
2024-06-03 12:03:48 +0200

    planner fix

--- types/Planner.js
+++ types/Planner.js
@@ -7,32 +7,29 @@ import { getByValue } from "../utils/functions/gameMap_utils.js";
 import { nearestDelivery } from "../utils/functions/distance.js";
 import { DEBUG } from "../config.js";
 import { PddlDomain, PddlAction, PddlProblem, PddlExecutor, onlineSolver, Beliefset } from "@unitn-asa/pddl-client";
+import BeliefSet from "./BeliefSet.js";
 
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-function readFile ( filePath ) {
-
+function readFile(filePath) {
     filePath = path.join(__dirname, filePath);
-    
-    return new Promise( (res, rej) => {
 
-        fs.readFile( filePath, 'utf8', (err, data) => {
-            if (err) rej(err)
-            else res(data)
-        })
-
-    })
-
+    return new Promise((res, rej) => {
+        fs.readFile(filePath, "utf8", (err, data) => {
+            if (err) rej(err);
+            else res(data);
+        });
+    });
 }
 
 class PddlAction_custom extends PddlAction {
     beliefSet;
-    constructor(beliefSet, ...args){
+    constructor(beliefSet, ...args) {
         super(args);
         this.beliefSet = beliefSet;
     }
@@ -70,12 +67,13 @@ export default class Planner {
     goal_pddlstring;
 
     /** @type {Array<string>} */
-    // supported_intentions = ["go_to", "go_deliver", "go_pick__up"];
-    supported_intentions = ["go_to"];
+    supported_intentions = ["go_to", "go_deliver", "go_pick_up"];
+    // supported_intentions = ["go_to"];
 
     current_plan;
 
-    
+    id;
+
     constructor(pddl) {
         this.planLibrary.push(GoDeliver);
         this.planLibrary.push(GoPickUp);
@@ -84,16 +82,16 @@ export default class Planner {
 
         this.pddl = pddl;
 
-        readFile('../pddl/domain-deliveroo.pddl')
-            .then(data => {
+        readFile("../pddl/domain-deliveroo.pddl")
+            .then((data) => {
                 // console.log('File content:', data);
                 this.domain = data;
-                if(DEBUG) console.log('Domain read successfully.');
+                if (DEBUG) console.log("Domain read successfully.");
             })
-            .catch(err => {
-                if(DEBUG) console.error('Error reading file:', err);
+            .catch((err) => {
+                if (DEBUG) console.error("Error reading file:", err);
             });
-            
+
         this.map_init_pddlstring = "";
         this.objects_init_pddlstring = "";
         this.goal_pddlstring = "";
@@ -124,29 +122,32 @@ export default class Planner {
 
     set_goal_pddlstring(beliefSet, predicate) {
         var dest_tile = getByValue(beliefSet.map.tiles, [predicate[1], predicate[2]]);
-        
+
         switch (predicate[0]) {
             case "go_to":
                 this.goal_pddlstring = `and (is_at tile_${dest_tile})`;
                 break;
-                case "go_deliver":
+            case "go_deliver":
                 let deliveryTile = nearestDelivery(beliefSet.me, beliefSet.map, beliefSet.graph);
                 var dest_tile = getByValue(beliefSet.map.tiles, [deliveryTile.x, deliveryTile.y]);
                 this.goal_pddlstring = `and (delivered_at tile_${dest_tile})`;
+                this.map_init_pddlstring += `(carrying parcel)`;
                 break;
             case "go_pick_up":
+                this.id = predicate[3];
                 this.map_init_pddlstring += `(parcel_at tile_${dest_tile}) `;
                 this.goal_pddlstring = `and (carrying parcel)`;
                 break;
             default:
-                throw("Predicate not supported");
+                throw "Predicate not supported";
                 break;
         }
     }
 
+    /** @param {BeliefSet} beliefSet */
     async execute_action(action, beliefSet) {
         try {
-            switch(action) {
+            switch (action) {
                 case "move_right":
                     await beliefSet.client.move("right");
                     break;
@@ -160,12 +161,12 @@ export default class Planner {
                     await beliefSet.client.move("down");
                     break;
                 case "pickup":
-                    let pickup = await this.beliefSet.client.pickup();
+                    let pickup = await beliefSet.client.pickup();
                     console.log(pickup);
                     if (!(pickup == []) && pickup.length > 0) {
-                        // if (DEBUG) console.log("picked up", id);
-                        beliefSet.me.carrying.set(id, this.beliefSet.parcels.get(id));
-                        beliefSet.parcels.get(id).carriedBy = this.beliefSet.me.id;
+                        beliefSet.me.carrying.set(this.id, beliefSet.parcels.get(this.id));
+                        beliefSet.parcels.get(this.id).carriedBy = beliefSet.me.id;
+                        if (DEBUG) console.log("picked up", this.id);
                     }
                     break;
                 case "putdown":
@@ -174,26 +175,23 @@ export default class Planner {
                     break;
             }
         } catch (error) {
-            throw("Error");
+            throw "Error while executing action with error:", error;
         }
     }
 
     async plan_to_actions(plan, beliefSet) {
+        if (!plan) return;
 
-        if ( ! plan )
-            return;
-        
-        var previousStepGoals = []
+        var previousStepGoals = [];
 
         for (const step of plan) {
             if (this.current_plan.stopped) throw ["stopped"]; // if stopped then quit
             if (step.parallel) {
-                console.log( 'Starting concurrent step', step.action, ...step.args )
-            }
-            else {
-                await Promise.all(previousStepGoals)
-                previousStepGoals = []
-                console.log( 'Starting sequential step', step.action, ...step.args )
+                console.log("Starting concurrent step", step.action, ...step.args);
+            } else {
+                await Promise.all(previousStepGoals);
+                previousStepGoals = [];
+                console.log("Starting sequential step", step.action, ...step.args);
             }
 
             let action = step.action.toLowerCase();
@@ -202,18 +200,24 @@ export default class Planner {
             //     continue;
             // }
 
-            var exec = this.execute_action(action, beliefSet);
-            if ( exec && exec.catch ) {
-                exec.catch( err => { throw err } ); //new Error('Step failed');
-                previousStepGoals.push( exec );
-            }
-            else {
-                previousStepGoals.push( Promise.resolve() );
+            try {
+                var exec = this.execute_action(action, beliefSet);
+                if (exec && exec.catch) {
+                    exec.catch((err) => {
+                        throw err;
+                    }); //new Error('Step failed');
+                    previousStepGoals.push(exec);
+                } else {
+                    previousStepGoals.push(Promise.resolve());
+                }
+            } catch (error) {
+                if (DEBUG) console.log("Error while executing action", action, "with error", error);
+                throw ("Error while executing action", action, "with error", error);
             }
         }
 
         // wait for last steps to complete before finish blackbox plan execution intention
-        await Promise.all(previousStepGoals)
+        await Promise.all(previousStepGoals);
     }
 
     async execute(beliefSet, predicate) {
@@ -221,20 +225,25 @@ export default class Planner {
             // restore beliefs
             this.map_init_pddlstring = this.map_init_pddlstring_backup;
             this.objects_init_pddlstring = this.objects_init_pddlstring_backup;
-            if(DEBUG) console.log("Executing pddl planner");
-            if(DEBUG) console.log("predicate", predicate);
+            if (DEBUG) console.log("Executing pddl planner");
+            if (DEBUG) console.log("predicate", predicate);
             this.add_agent_position(beliefSet);
             this.add_tiles_blocked_by_agents(beliefSet);
             this.set_goal_pddlstring(beliefSet, predicate);
 
-            this.problem = new PddlProblem("deliveroo", this.objects_init_pddlstring, this.map_init_pddlstring, this.goal_pddlstring);
+            this.problem = new PddlProblem(
+                "deliveroo",
+                this.objects_init_pddlstring,
+                this.map_init_pddlstring,
+                this.goal_pddlstring
+            );
 
-            // if(DEBUG) console.log(this.problem.toPddlString());
-            // if(DEBUG) console.log(this.domain);
+            // if (DEBUG) console.log(this.problem.toPddlString());
+            // if (DEBUG) console.log(this.domain);
             var plan = await onlineSolver(this.domain, this.problem.toPddlString());
-            if(DEBUG) console.log(plan);
+            if (DEBUG) console.log(plan);
 
-            if(plan == []) throw("No valid plan available.");
+            if (plan == []) throw "No valid plan available.";
 
             // this.init_executor(beliefSet);
             // try {
@@ -243,22 +252,20 @@ export default class Planner {
             // } catch (error) {
             //     throw("Error in executing pddl planner");
             // }
-            
+
             await this.plan_to_actions(plan, beliefSet);
 
             return true;
-
         } catch (error) {
-            if(DEBUG) console.log(error);
-            throw("Error in fetching plan with onlineSolver");
+            if (DEBUG) console.log(error);
+            throw "Error in fetching plan with onlineSolver";
         }
     }
 }
 
-
 // function init_executor(beliefSet) {
 //             // available actions: beliefSet.client.move("right")  right, left, up, down, this.beliefSet.client.pickup(), this.beliefSet.client.putdown()
-    
+
 //             const move_right = new PddlAction(
 //                 'move_right',
 //                 '',
